0ee2d0d6a7acbf5cfb8630ef4d00204de60d1867
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2018-02-02 16:54:06 -0200

    querystring on add, update and delete

--- src/Path.php
+++ src/Path.php
@@ -34,12 +34,12 @@ class Path
 
     /**
      * Returns all items (Method GET)
-     * @param array $params
+     * @param array $query
      * @return array
      */
-    public function getAll(array $params = []): array
+    public function getAll(array $query = []): array
     {
-        return $this->request->do('GET', $this->baseUrl, $params, $this->client->session()->headers);
+        return $this->request->do('GET', $this->baseUrl, $query, $this->client->session()->headers);
     }
 
     /**
@@ -55,31 +55,57 @@ class Path
     /**
      * Append a item to collection (Method POST)
      * @param array $data
-     * @return array
+     * @return array|null
      */
-    public function add(array $data): array
+    public function add(array $data, array $query = [])/*: ?array */
     {
-        return $this->request->do('POST', $this->baseUrl, $data, $this->client->session()->headers);
+        $url = $this->generateUrl($this->baseUrl, $query);
+        
+        return $this->request->do('POST', $url, $data, $this->client->session()->headers);
     }
 
     /**
      * Update existing item (Method PUT)
      * @param mixed $id
      * @param array $data
-     * @return array
+     * @return array|null
      */
-    public function update($id, array $data): array
+    public function update($id, array $data, array $query = [])/*: ?array */
     {
-        return $this->request->do('PUT', "{$this->baseUrl}/{$id}", $data, $this->client->session()->headers);
+        $url = $this->generateUrl("{$this->baseUrl}/{$id}", $query);
+        
+        return $this->request->do('PUT', $url, $data, $this->client->session()->headers);
     }
 
     /**
      * Delete existing item (Method DELETE)
      * @param mixed $id
-     * @return array
+     * @return array|null
      */
-    public function delete($id): array
+    public function delete($id, array $query = [])/*: ?array */
     {
-        return $this->request->do('DELETE', "{$this->baseUrl}/{$id}", [], $this->client->session()->headers);
+        $url = $this->generateUrl("{$this->baseUrl}/{$id}", $query);
+        
+        return $this->request->do('DELETE', $url, [], $this->client->session()->headers);
+    }
+    
+    /**
+     * Generate URL with querystring
+     * @param string $url
+     * @param array $query
+     * @return string
+     */
+    private function generateUrl(string $url, array $query): string
+    {
+        if (count($query)) {
+            if (strpos($url, '?') === false) {
+                $separator = '?';
+            } else {
+                $separator = '&';
+            }
+            $url .= $separator . http_build_query($query);
+        }
+        
+        return $url;
     }
 }
